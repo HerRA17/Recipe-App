a38a5fc24d9e30600b21d40bbbe7b2d0ca2409cb
Hermann Rasch <hermann.rasch93@gmail.com>
2023-11-21 11:28:02 +0100

    Update settings.py

--- src/recipe_app/settings.py
+++ src/recipe_app/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'recipe_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST':config('DB_HOST'),
-        'PORT':config('DB_PORT')
-    } 
+        'NAME': BASE_DIR / 'db.sqlite3' 
+              } 
 }
 
 # Password validation
