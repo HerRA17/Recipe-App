bf9db24bb33e9013dcc61fd5eab91180ff6afee6
Hermann Rasch <hermann.rasch93@gmail.com>
2023-11-22 21:00:41 +0100

    Testing if this part is required for Heroku to use the db

--- src/recipe_app/settings.py
+++ src/recipe_app/settings.py
@@ -143,7 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
