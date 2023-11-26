2b439779382c1c1e2de5f0e450d52a9429fe7c73
Hermann Rasch <hermann.rasch93@gmail.com>
2023-11-26 15:44:47 +0100

    Update settings.py

--- recipe_app/settings.py
+++ recipe_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 
 DEBUG=False
 
-ALLOWED_HOSTS = ['https://recipe-app-new-ccb69d876db5.herokuapp.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['recipe-app-new-ccb69d876db5.herokuapp.com/', '127.0.0.1', 'localhost']
 
 
 # Application definition
